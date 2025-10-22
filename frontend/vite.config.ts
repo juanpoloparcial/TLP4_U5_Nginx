01b1b94b34c1f68df2d1d3dbd0020d70cbd08fad
JuanchoStock <juanchostock@gmail.com>
2025-10-22 16:32:05 -0300

    TLP4-unidad 5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  },
-  build: {
-    outDir: 'dist'
+export default defineConfig(async () => {
+  const reactPlugin = await import('@vitejs/plugin-react')
+  return {
+    plugins: [
+      (reactPlugin.default ?? reactPlugin)(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000/',
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
   }
 })
